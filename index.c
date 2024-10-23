fe183979a2f5d0b9ca437b2e62a7566b08313c36
shibsrc <shibjyoti252@gmail.com>
2024-10-23 22:12:57 +0530

    a logic for substring  applied

--- index.c
+++ index.c
@@ -2,7 +2,42 @@
 #include<stdlib.h>
 #include<string.h>
 
-int main(){
+char* substring(char* str,int s,int e){
+    char * substr = (char*)malloc((e*sizeof(char)));
+    strncpy(substr,str+s,e);
+    substr[e]='\0';
 
+    return substr;
+}
+char* postfix(char str[]){
+    int l =strlen(str);
+    for(int i=0; i < l;i++){
+        if(str[i]=='/' || str[i]=='*'){
+            char *a =substring(str,0,i);
+            char* b =substring(str,i+1,l-1);
+            
+            if(str[i]=='/'){
+                char * c= strcat(postfix(a),postfix(b));
+                char * d= strcat(c,"/");
+                return(d);
+                free(c);
+                free(d);
+            }else{
+                char * c= strcat(postfix(a),postfix(b));
+                char * d= strcat(c,"*");
+                return(d);
+                free(c);
+                free(d);
+            }
+        }
+    }
+    return str;
+}
+int main(){
+    char str[100];
+    printf("Enter an Expression\t");
+    scanf("%s",str);
+    char* p = postfix(str);
+    printf("The post Expression from main is %s",p);
     return 0;
 }
